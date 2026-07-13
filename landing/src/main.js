e5ad2ff3119b794e4875ab7be1d55b205cc3c5e5
ByteShark-dev <byteshark098@gmail.com>
2026-07-12 20:40:05 -0600

    chore(repo): move website to standalone repository

--- landing/src/main.js
+++
@@ -1,7 +0,0 @@
-import { mountPage } from './app.js';
-import { renderLandingPage } from './components/LandingPage.js';
-
-mountPage({
-  pageKey: 'home',
-  render: renderLandingPage,
-});
--- landing/src/privacy.js
+++
@@ -1,7 +0,0 @@
-import { mountPage } from './app.js';
-import { renderPrivacyPage } from './components/PrivacyPage.js';
-
-mountPage({
-  pageKey: 'privacy',
-  render: renderPrivacyPage,
-});
